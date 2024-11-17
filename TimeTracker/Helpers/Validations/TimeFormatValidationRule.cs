f376615885bc751022e770696abe42935603b7cc
M-Str <miroslav.stransky@live.com>
2024-11-17 23:57:42 +0100

    MOD: přidány validace na timeformat

---
+++ TimeTracker/Helpers/Validations/TimeFormatValidationRule.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace TimeTracker.Helpers.Validations
+{
+	public class TimeFormatValidationRule : ValidationRule
+	{
+		public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+		{
+			if (value == null || string.IsNullOrWhiteSpace(value.ToString()))
+			{
+				return new ValidationResult(false, "Čas je povinný.");
+			}
+
+			if (TimeSpan.TryParseExact(value.ToString(), "hh\\:mm\\:ss", cultureInfo, out _))
+			{
+				return ValidationResult.ValidResult;
+			}
+
+			return new ValidationResult(false, "Čas musí být ve formátu HH:mm:ss.");
+		}
+	}
+}
