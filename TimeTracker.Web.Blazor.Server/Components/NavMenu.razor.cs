eedd56b4a73a09acbc3b0fcc6387629ee26ebaaf
M-Str <miroslav.stransky@live.com>
2025-03-02 23:58:29 +0100

    MOD: upravený styl navMenu

---
+++ TimeTracker.Web.Blazor.Server/Components/NavMenu.razor.cs
@@ -0,0 +1,21 @@
+﻿using TimeTracker.BE.Web.Shared.Models;
+
+namespace TimeTracker.Web.Blazor.Server.Components
+{
+	public partial class NavMenu
+	{
+		BuildInfo _buildInfo = new BuildInfo();
+		private bool collapseNavMenu = true;
+
+		private string? NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+		private void ToggleNavMenu()
+		{
+			collapseNavMenu = !collapseNavMenu;
+		}
+
+		private void onOpenDocumentPageClick() => NavigationManager.NavigateTo("/swagger/index.html", true);
+
+		private void onOpenEventLogsPageClick() => NavigationManager.NavigateTo("/event-logs");
+	}
+}
