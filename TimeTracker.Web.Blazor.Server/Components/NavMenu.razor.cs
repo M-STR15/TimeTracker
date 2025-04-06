3b31620c9526ad11266da46aed3bd5b6c44c1f2e
M-Str <miroslav.stransky@live.com>
2025-04-06 13:14:28 +0200

    MOD: odstranění zbytečného tlačítka z navigačního menu

--- TimeTracker.Web.Blazor.Server/Components/NavMenu.razor.cs
+++ TimeTracker.Web.Blazor.Server/Components/NavMenu.razor.cs
@@ -15,7 +15,6 @@ namespace TimeTracker.Web.Blazor.Server.Components
 		}
 
 		private void onOpenDocumentPage_Click() => NavigationManager.NavigateTo("/swagger/index.html", true);
-		private void onOpenEventLogsPage_Click() => NavigationManager.NavigateTo("/event-logs");
 		private void onOpenCurrentActivityPage_Click() => NavigationManager.NavigateTo("/current-activity");
 		private void onOpenSettings_Click() => NavigationManager.NavigateTo("/settings");
 		private void onOpenShifts_Click() => NavigationManager.NavigateTo("/shifts");
