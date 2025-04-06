a2f85a54fb7a3418a6bb8a76116348824cc2d3c8
M-Str <miroslav.stransky@live.com>
2025-04-06 15:26:57 +0200

    MOD: úprava report Controlleru, přidání dalšímethody pro předaání reportu

--- TimeTracker.BE.Web.BusinessLogic/Controllers/ReportController.cs
+++ TimeTracker.BE.Web.BusinessLogic/Controllers/ReportController.cs
@@ -23,8 +23,35 @@ namespace TimeTracker.BE.Web.BusinessLogic.Controllers
 		{
 			try
 			{
-				var dateStart = new DateTime(2025, 1, 1);
+				var dateStart = DateTime.MinValue;
 				var dateEnd = DateTime.Now;
+				var recordActivities = await _recordRepository.GetRecordsAsync(dateStart, dateEnd);
+				if (recordActivities != null)
+				{
+					var recordActivitiesDto = _mapper.Map<List<RecordActivityDetailDto>>(recordActivities);
+					return recordActivitiesDto != null ? Ok(recordActivitiesDto) : Problem();
+				}
+				else
+				{
+					return NotFound();
+				}
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+			}
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="dateStart">datum musí být ve formátu ISO 8601 např. 2025-04-06</param>
+		/// <param name="dateEnd">datum musí být ve formátu ISO 8601 např. 2025-04-06</param>
+		/// <returns></returns>
+		[HttpGet("api/v1/reports/record-activiries/{dateStart}/{dateEnd}")]
+		public async Task<ActionResult<List<TypeShiftBaseDto>>> GetRecordActivitiesDetailAsync([FromRoute] DateTime dateStart, [FromRoute] DateTime dateEnd)
+		{
+			try
+			{
 				var recordActivities = await _recordRepository.GetRecordsAsync(dateStart, dateEnd);
 				if (recordActivities != null)
 				{
---
+++ TimeTracker.Web.Blazor.Server/Helpers/GeneratorDatesHelper.cs
@@ -0,0 +1,43 @@
+﻿using TimeTracker.BE.Web.BusinessLogic.Models.DTOs;
+using TimeTracker.Web.Blazor.Server.Models;
+
+namespace TimeTracker.Web.Blazor.Server.Helpers
+{
+	internal static class GeneratorDatesHelper
+	{
+		internal static IEnumerable<MonthAndYearItemViewModel> GetMontData()
+		{
+			List<MonthAndYearItemViewModel> monthAndYears = new();
+			var startDate = new DateTime(2025, 1, 1);
+			var endDate = DateTime.Today;
+
+			while (startDate <= endDate)
+			{
+				monthAndYears.Add(new MonthAndYearItemViewModel(startDate));
+				startDate = startDate.AddMonths(1);
+			}
+
+			return monthAndYears.OrderByDescending(x => x.Date);
+		}
+
+		internal static IEnumerable<ShiftViewModel> GetDaysInMonth(MonthAndYearItemViewModel item, List<ShiftBaseDto> planShiftsInDB, List<TypeShiftBaseDto> typeShifts)
+		{
+			List<ShiftViewModel> _days = new();
+			var daysInMonth = DateTime.DaysInMonth(item.Year, item.Month);
+			for (int i = 0; i < daysInMonth; i++)
+			{
+				var startDate = new DateTime(item.Year, item.Month, i + 1);
+				var shiftInDb = planShiftsInDB.FirstOrDefault(x => x.StartDate == startDate);
+				var existShift = (shiftInDb != null);
+
+				Guid guidId = existShift ? shiftInDb.GuidId : Guid.Empty;
+				int typeShiftId = existShift ? shiftInDb.TypeShiftId : 0;
+				string? description = existShift ? shiftInDb?.Description : null;
+				var typeShift = typeShifts.FirstOrDefault(x => x.Id == typeShiftId);
+				_days.Add(new ShiftViewModel(guidId, startDate, typeShiftId, typeShift, description));
+			}
+
+			return _days;
+		}
+	}
+}
