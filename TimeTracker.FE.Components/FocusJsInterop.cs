624e5cd4cd8609bb99d22deb96cb4980696f465d
M-Str <miroslav.stransky@live.com>
2025-04-11 22:23:11 +0200

    MOD

---
+++ TimeTracker.FE.Components/FocusJsInterop.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace TimeTracker.FE.Components;
+
+// This class provides an example of how JavaScript functionality can be wrapped
+// in a .NET class for easy consumption. The associated JavaScript module is
+// loaded on demand when first needed.
+//
+// This class can be registered as scoped DI service and then injected into Blazor
+// components for use.
+
+public class FocusJsInterop : IAsyncDisposable
+{
+    private readonly Lazy<Task<IJSObjectReference>> _moduleTask;
+
+	public FocusJsInterop(IJSRuntime jsRuntime)
+    {
+		_moduleTask = new (() => jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/TimeTracker.FE.Components/js/focusUtils.js").AsTask());
+    }
+
+	public async ValueTask DelayBlur<TComponent>(DotNetObjectReference<TComponent> componentRef) where TComponent : class
+	{
+		var module = await _moduleTask.Value;
+		await module.InvokeVoidAsync("delayBlur", componentRef);
+	}
+
+	public async ValueTask RemoveFocus(ElementReference element)
+	{
+		var module = await _moduleTask.Value;
+		await module.InvokeVoidAsync("removeFocus", element);
+	}
+
+	public async ValueTask DisposeAsync()
+	{
+		if (_moduleTask.IsValueCreated)
+		{
+			var module = await _moduleTask.Value;
+			await module.DisposeAsync();
+		}
+	}
+}
