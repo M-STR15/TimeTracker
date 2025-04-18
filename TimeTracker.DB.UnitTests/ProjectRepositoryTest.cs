1e1a1395df6b716ef6d238cd27ac8128b9efea91
M-Str <miroslav.stransky@live.com>
2025-04-18 11:10:10 +0200

    MOD: vytvoření abstraktní třídy pro testoání

--- TimeTracker.DB.UnitTests/ProjectRepositoryTest.cs
+++ TimeTracker.DB.UnitTests/ProjectRepositoryTest.cs
@@ -1,7 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using Moq;
-using Ninject.Activation;
-using System.ComponentModel.DataAnnotations;
 using TimeTracker.BE.DB.DataAccess;
 using TimeTracker.BE.DB.Models.Entities;
 using TimeTracker.BE.DB.Models.Interfaces;
@@ -10,17 +8,11 @@ using TimeTracker.DB.UnitTests.Models;
 
 namespace TimeTracker.DB.UnitTests
 {
-	public class ProjectRepositoryTest
+	public class ProjectRepositoryTest : aRepositoryBaseTest
 	{
-		private readonly Mock<IDbContextFactory<MsSqlDbContext>> _contextFactoryMock;
-		private readonly DbContextOptions<MsSqlDbContext> _dbOptions;
 		private readonly ProjectRepository<MsSqlDbContext> _projectRepository;
-		public ProjectRepositoryTest()
+		public ProjectRepositoryTest() : base()
 		{
-			_contextFactoryMock = new Mock<IDbContextFactory<MsSqlDbContext>>();
-			_dbOptions = new DbContextOptionsBuilder<MsSqlDbContext>()
-							.UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()) // Unikátní DB pro každý test
-							.Options;
 			_projectRepository = new ProjectRepository<MsSqlDbContext>(() => new MsSqlDbContext(_dbOptions));
 		}
 
@@ -199,37 +191,6 @@ namespace TimeTracker.DB.UnitTests
 					};
 
 			compareAllProperties<IProjectBase>(inputProject, outputProejct, ignoredProperties);
-		}
-
-		private async Task<MsSqlDbContext> createContextAsync()
-		{
-			var context = new MsSqlDbContext(_dbOptions);
-			await context.Database.EnsureCreatedAsync();
-			_contextFactoryMock.Setup(x => x.CreateDbContextAsync(It.IsAny<CancellationToken>()))
-				.ReturnsAsync(context);
-			return context;
-		}
-
-		/// <summary>
-		/// Pøed každým testem databázi explicitnì odstraòte a znovu vytvoøte
-		/// </summary>
-		/// <param name="context"></param>
-		private async Task resetDBAsync(MsSqlDbContext context)
-		{
-			await context.Database.EnsureDeletedAsync();
-			await context.Database.EnsureCreatedAsync();
-		}
-		private void compareAllProperties<T>(T expectedObj, T actualObj, HashSet<string>? ignoredProperties = null)
-		{
-			foreach (var prop in typeof(T).GetProperties())
-			{
-				if (ignoredProperties != null && ignoredProperties.Contains(prop.Name))
-					continue;
-
-				var expected = prop.GetValue(expectedObj);
-				var actual = prop.GetValue(actualObj);
-				Assert.Equal(expected, actual);
-			}
 		}
 	}
 }
---
+++ TimeTracker.DB.UnitTests/aRepositoryBaseTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using TimeTracker.BE.DB.DataAccess;
+
+namespace TimeTracker.DB.UnitTests
+{
+	public abstract class aRepositoryBaseTest
+	{
+		protected readonly Mock<IDbContextFactory<MsSqlDbContext>> _contextFactoryMock;
+		protected readonly DbContextOptions<MsSqlDbContext> _dbOptions;
+
+		public aRepositoryBaseTest()
+		{
+			_contextFactoryMock = new Mock<IDbContextFactory<MsSqlDbContext>>();
+			_dbOptions = new DbContextOptionsBuilder<MsSqlDbContext>()
+							.UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()) // Unikátní DB pro každý test
+							.Options;
+		}
+
+		protected async Task<MsSqlDbContext> createContextAsync()
+		{
+			var context = new MsSqlDbContext(_dbOptions);
+			await context.Database.EnsureCreatedAsync();
+			_contextFactoryMock.Setup(x => x.CreateDbContextAsync(It.IsAny<CancellationToken>()))
+				.ReturnsAsync(context);
+			return context;
+		}
+
+		/// <summary>
+		/// Před každým testem databázi explicitně odstraňte a znovu vytvořte
+		/// </summary>
+		/// <param name="context"></param>
+		protected async Task resetDBAsync(MsSqlDbContext context)
+		{
+			await context.Database.EnsureDeletedAsync();
+			await context.Database.EnsureCreatedAsync();
+		}
+		protected void compareAllProperties<T>(T expectedObj, T actualObj, HashSet<string>? ignoredProperties = null)
+		{
+			foreach (var prop in typeof(T).GetProperties())
+			{
+				if (ignoredProperties != null && ignoredProperties.Contains(prop.Name))
+					continue;
+
+				var expected = prop.GetValue(expectedObj);
+				var actual = prop.GetValue(actualObj);
+				Assert.Equal(expected, actual);
+			}
+		}
+	}
+}
