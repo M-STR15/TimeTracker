20c19c6e16994fe85c9cb10e26c345b02be60f4d
M-Str <miroslav.stransky@live.com>
2025-04-18 20:03:57 +0200

    ADD: vytvoření uni testu pro TypeShiftRepository

---
+++ TimeTracker.DB.UnitTests/TypeShiftRepositoryTest.cs
@@ -0,0 +1,57 @@
+using TimeTracker.Basic.Enums;
+using TimeTracker.BE.DB.DataAccess;
+using TimeTracker.BE.DB.Models.Entities;
+using TimeTracker.BE.DB.Repositories;
+
+namespace TimeTracker.DB.UnitTests
+{
+	public class TypeShiftRepositoryTest : aRepositoryBaseTest
+	{
+		private readonly TypeShiftRepository<MsSqlDbContext> _typeShiftRepository;
+
+		public TypeShiftRepositoryTest() : base()
+		{
+			_typeShiftRepository = new TypeShiftRepository<MsSqlDbContext>(() => new MsSqlDbContext(_dbOptions));
+		}
+
+		[Fact]
+		public async Task GetAllAsync_ShouldReturnAllTypeShifts()
+		{
+			// Arrange
+			await using var context = await createContextAsync();
+			await resetDBAsync(context);
+			// Act
+			IEnumerable<TypeShift> result = await _typeShiftRepository.GetAllAsync();
+			Array enums = Enum.GetValues(typeof(eTypeShift));
+			int countEnums = enums.Length;
+			// Assert
+			Assert.NotNull(result);
+			Assert.Equal(countEnums, result.Count());
+
+			var resultIds = result.Select(r => r.Id).ToHashSet();
+			var enumIds = Enum.GetValues(typeof(eTypeShift)).Cast<eTypeShift>().Select(e => (int)e).ToHashSet();
+
+			Assert.True(resultIds.SetEquals(enumIds));
+		}
+
+		[Fact]
+		public async Task GetTypeShiftsForMainWindowAsync_ShouldReturnVisibleTypeShifts()
+		{
+			// Arrange
+			await using var context = await createContextAsync();
+			await resetDBAsync(context);
+
+			// Act
+			var result = await _typeShiftRepository.GetTypeShiftsForMainWindowAsync();
+
+			// Assert
+			Assert.NotNull(result);
+
+
+			var resultIds = result.Select(r => r.Id).ToHashSet();
+			var enumIds = Enum.GetValues(typeof(eTypeShift)).Cast<eTypeShift>().Where(x => x != eTypeShift.Holiday).Select(e => (int)e).ToHashSet();
+
+			Assert.True(resultIds.SetEquals(enumIds));
+		}
+	}
+}
